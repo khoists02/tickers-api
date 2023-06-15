4c3b96de919c4eb0e28c093223cecfebffb9357a
khoi.le <khoi.le@advahealthsolutions.com>
2023-06-15 08:16:01 +0700

    add ticker types enums

---
+++ src/main/java/com/tickers/io/applicationapi/enums/TickerTypesEnum.java
@@ -0,0 +1,28 @@
+package com.tickers.io.applicationapi.enums;
+
+public enum TickerTypesEnum {
+    CS,
+    PFD,
+    WARRANT,
+    RIGHT,
+    BOND,
+    ETF,
+    ETN,
+    ETV,
+    SP,
+    ADRC,
+    ADRP,
+    ADRW,
+    ADRR,
+    FUND,
+    BASKET,
+    UNIT,
+    LT,
+    OS,
+    GDR,
+    OTHER,
+    NYRS,
+    AGEN,
+    EQLK,
+    ETS
+}
