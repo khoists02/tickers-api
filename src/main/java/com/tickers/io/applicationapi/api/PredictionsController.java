d5c68b3badac0d18394eafab4c37e9aace2483bd
khoi.le <khoi.le@advahealthsolutions.com>
2023-06-28 14:30:26 +0700

    handle exception

--- src/main/java/com/tickers/io/applicationapi/api/PredictionsController.java
+++ src/main/java/com/tickers/io/applicationapi/api/PredictionsController.java
@@ -1,6 +1,7 @@
 package com.tickers.io.applicationapi.api;
 
 import com.tickers.io.applicationapi.api.exceptions.PredictionsException;
+import com.tickers.io.applicationapi.exceptions.BadRequestException;
 import com.tickers.io.applicationapi.exceptions.NotFoundException;
 import com.tickers.io.applicationapi.model.Predictions;
 import com.tickers.io.applicationapi.model.TickerDetails;
@@ -17,8 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/predictions")
@@ -36,6 +38,28 @@ public class PredictionsController {
     public void createPredictionFilter(@RequestBody @Valid PredictionsProto.PredictionsRequest request) {
        try {
            // Check date filter in range
+           if (request.getTrainFilter().isBlank() || request.getTrainFilter().isEmpty()) throw new BadRequestException();
+           if (request.getTestFilter().isBlank() || request.getTestFilter().isEmpty()) throw new BadRequestException();
+
+           List<String> trainDateStrArr = Arrays.asList(request.getTrainFilter().split(","));
+           List<ZonedDateTime> trainDates = trainDateStrArr.stream().map((x -> ZonedDateTime.parse(x))).collect(Collectors.toList());
+
+           ZonedDateTime startTrain = trainDates.get(0);
+           ZonedDateTime endTrain = trainDates.get(1);
+
+           if (startTrain != null && endTrain != null && (startTrain.isAfter(endTrain) || startTrain.isEqual(endTrain)))
+               throw PredictionsException.START_DATE_MUST_AFTER_END_DATE;
+
+           List<String> testDateStrArr = Arrays.asList(request.getTestFilter().split(","));
+           List<ZonedDateTime> testDates = testDateStrArr.stream().map((x -> ZonedDateTime.parse(x))).collect(Collectors.toList());
+
+           ZonedDateTime startTest = testDates.get(0);
+           ZonedDateTime endTest = testDates.get(1);
+
+
+           if (startTest != null && endTest != null && (startTest.isAfter(endTest) || startTest.isEqual(endTest)))
+               throw PredictionsException.START_DATE_MUST_AFTER_END_DATE;
+
 
            TickerDetails tickerDetails = tickerDetailsRepository.findById(UUID.fromString(request.getTickerDetailsId())).orElseThrow(NotFoundException::new);
            Predictions predictions = new Predictions();
